f52efc28ac2073e61204ebf69e990f2355b07b78
Rahul <78337705+Rahul96108@users.noreply.github.com>
2026-01-23 18:41:05 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,25 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from .models import db
 # Initialize the database object globally
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
-    # --- 1. CONFIGURATION ---
-    # In production, the cloud provides a 'PORT' and 'DATABASE_URL'
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev_key_123')
     app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'uploads')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # --- 2. AUTOMATIC FOLDER CREATION ---
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
 
-    # --- 3. INITIALIZE PLUGINS ---
     db.init_app(app)
 
-    # --- 4. REGISTER BLUEPRINTS ---
-    # We import inside the function to avoid "Circular Imports"
     from .routes import main_bp
     app.register_blueprint(main_bp)
 
     return app
+
